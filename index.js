d49310fd739cb1626aece657c8fae3daa064e84e
Azril Achmad <azrilachmad4@gmail.com>
2024-07-31 20:31:46 +0700

    adjust index.js

--- index.js
+++ index.js
@@ -3,19 +3,32 @@ import bodyParser from 'body-parser'
 import cors from 'cors';
 import vehiclesRoutes from './routes/vehicles.js'
 import db from './config/db.js'
+import * as http from 'http';
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
-const PORT = 5000;
 
-
-app.use(cors({origin: '*'}));
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 db.authenticate()
     .then(() => console.log('Database Connected...'))
     .catch(err => console.error("Error connecting to the database: ", err))
 
-
 app.use(vehiclesRoutes);
 
+const hostname = process.env.HOSTNAME;
 
-app.listen(PORT, () => console.log(`listening on port: http://localhost:${PORT}`));
+const port = 3001;
+
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World!\n');
+});
+
+server.listen(port, hostname, () => {           
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+
