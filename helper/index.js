ac07372b0b2ac24d5801dedfa7c0231e5ce15e9c
Azril Achmad <azrilachmad4@gmail.com>
2025-01-03 16:16:15 +0700

    Create Cron job for AI Price Check

--- helper/index.js
+++ helper/index.js
@@ -1,19 +1,34 @@
-const dayjs =  require ("dayjs");
+const dayjs = require("dayjs");
 
 const convDate = (date, customFormat) => {
-    const convert = () => {
-      return dayjs(date).format(customFormat ? customFormat : 'YYYY-MM-DD');
-    };
-  
-    if (date && date instanceof Date && !isNaN(date.valueOf())) {
-      return convert();
-    } else {
-      const check = dayjs(date).isValid();
-      return check ? convert() : null;
-    }
+  const convert = () => {
+    return dayjs(date).format(customFormat ? customFormat : 'YYYY-MM-DD');
   };
+
+  if (date && date instanceof Date && !isNaN(date.valueOf())) {
+    return convert();
+  } else {
+    const check = dayjs(date).isValid();
+    return check ? convert() : null;
+  }
+};
+
+const msToHHMMSS = (ms) => {
+  const totalSeconds = Math.floor(ms / 1000);
+  const hours = Math.floor(totalSeconds / 3600);
+  const minutes = Math.floor((totalSeconds % 3600) / 60);
+  const seconds = totalSeconds % 60;
+
+  // Pad hours, minutes, and seconds with leading zeros
+  return [
+    String(hours).padStart(2, '0'),
+    String(minutes).padStart(2, '0'),
+    String(seconds).padStart(2, '0'),
+  ].join(':');
+};
 
 module.exports = {
   convDate,
+  msToHHMMSS
 
 }
--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 const globalErrorHandler = require('./controllers/errorController.js')
 const cron = require('node-cron');
 const jobSchedule = require('./db/sqModels/jobSchedule.js')
-const {convDate} = require('./helper/index.js')
+const { convDate } = require('./helper/index.js')
 
 // Define every route
 const vehicleRoute = require('./routes/vehicleRoute.js')
@@ -19,14 +19,24 @@ const authRoute = require('./routes/authRoute.js')
 const userRoute = require('./routes/userRoute.js')
 const dataParameterRoute = require('./routes/dataParameterRoute.js')
 const dataSourceRoute = require('./routes/dataSourceRoute.js')
-const jobScheduleRoute = require('./routes/jobScheduleRoute.js')
+const jobScheduleRoute = require('./routes/jobScheduleRoute.js');
+const Cars = require('./model/vehicleModel.js');
+const dataParameter = require('./db/sqModels/dataParameter.js');
+const { GoogleGenerativeAI } = require('@google/generative-ai');
+const dataSource = require('./db/sqModels/dataSource.js');
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-flash",
+    generationConfig: { "response_mime_type": "application/json" },
+});
 
 
 
 
 app.use(cors({
     credentials: true,
-    origin:'*'
+    origin: '*'
 }));
 app.use(express.json());
 
@@ -47,12 +57,98 @@ app.use(jobScheduleRoute);
         const jobScheduleData = await jobSchedule.findAll();
         const parseData = jobScheduleData.map((item) => item.toJSON());
 
-        const hour =  convDate(parseData[0].time, 'hh') 
-        const minute = convDate(parseData[0].time, 'mm')
-        const second = convDate(parseData[0].time, 'ss')
+        const hour = convDate(parseData[0].time, 'hh');
+        const minute = convDate(parseData[0].time, 'mm');
+        const second = convDate(parseData[0].time, 'ss');
 
-        cron.schedule(`${second} * * * * *`, async() => {
-            
+        // Inisiasi CRON berdasarkan waktu dari setting schedule Web UI
+        cron.schedule(`10,20,30,40,50 * * * * *`, async () => {
+            console.log('Cron job started');
+            const startTime = Date.now();
+            // Proses query data di DB berdasarkan max_record setting
+            const rawData = await Cars.findAndCountAll({
+                limit: parseData[0].max_record,
+                offset: 0,
+                order: [['updated_at', 'ASC']],
+                where: {
+                    harga_atas: null,
+                    harga_bawah: null,
+                }
+            });
+
+            // Store dataset ke array
+            let dataSet = [];
+            rawData.rows.map((item) => {
+                dataSet.push(item.dataValues);
+            });
+            // console.log(dataSet);
+
+            // Mendapatkan dynamic prompt berdasarkan setting data parameter dari database
+            const dataParam = await dataParameter.findAndCountAll({
+                where: {
+                    status: true,
+                }
+            });
+
+            let parameterSet = {};
+            dataParam.rows.map((item) => {
+                parameterSet = {
+                    ...parameterSet,
+                    [item.dataValues.table_column]: item.dataValues.parameter,
+                };
+            });
+            // console.log(parameterSet);
+
+            // Mendapatkan dynamic prompt berdasarkan setting data source dari database
+            const dataSourceData = await dataSource.findAndCountAll({
+                where: {
+                    status: true,
+                }
+            });
+
+            let sourceSet = [];
+            dataSourceData.rows.map((item) => {
+                sourceSet.push(item.dataValues.address);
+            });
+            // console.log(sourceSet);
+            let totalToken = 0
+            // Proses Prompting AI (Price Check) berdasarkan dataSet
+            for (const data of dataSet) {
+                const parameterString = Object.entries(parameterSet)
+                    .map(([key, value]) => `${value}: ${data[key]}`)
+                    .join(", ");
+
+                const referenceLinks = sourceSet
+                    .map((link) => `- ${link}`)
+                    .join(", ");
+
+                const prompt = `Berikan Average Market Price untuk ${parameterString}. pastikan output harus sesuai dengan format json sebagai berikut: {"harga_terendah": Harga Terendah, "harga_tertinggi": Harga Tertinggi}.`;
+
+                console.log(prompt);
+
+                try {
+                    // Menggunakan await untuk memastikan prompting selesai sebelum melanjutkan ke iterasi berikutnya
+                    const promptResult = await model.generateContent(prompt);
+                    console.log(promptResult.response.text());
+                    totalToken += promptResult.response.usageMetadata.totalTokenCount * 1
+                    console.log(promptResult.response.usageMetadata.totalTokenCount);
+
+                    const endTime = Date.now();
+                    const executionTime = endTime - startTime; msto
+
+                    // const { harga_terendah, harga_tertinggi } = response.data;
+
+                    // Update harga_atas dan harga_bawah pada tabel Cars
+                    // await Cars.update(
+                    //     { harga_atas: harga_tertinggi, harga_bawah: harga_terendah },
+                    //     { where: { id: data.id } }
+                    // );
+
+                } catch (error) {
+                    console.error(`Error fetching price for car ID ${data.id}:`, error);
+                }
+                console.log('Total Token = ' + totalToken)
+            }
         });
     } catch (error) {
         console.error("Error occurred:", error);
