6c914a976a341f571cc3df0a43bd0638b0c2e68a
Azril Achmad <azrilachmad4@gmail.com>
2025-01-10 03:10:49 +0700

    Create API AI Token Usage per Schedule

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -4,6 +4,7 @@ const { convDate } = require("../helper/index.js");
 const catchAsync = require('../utils/catchAsync.js');
 const sequelize = require("../config/db.js");
 const { DataTypes, Op, Sequelize } = require("sequelize");
+const scheduleLog = require('../db/sqModels/scheduleLog.js');
 
 
 const getAllVehicleCount = catchAsync(async (req, res) => {
@@ -59,8 +60,37 @@ const getProcessedData = catchAsync(async (req, res) => {
     }
 })
 
+const getLogData = (async (req, res) => {
+
+    
+    try {
+        const {startDate, endDate} = req.query
+        
+        const chart1Data = await scheduleLog.findAndCountAll({
+            where: {
+                date: {
+                    [Op.between]: [new Date(startDate).setHours(0, 0, 0), new Date(endDate).setHours(23, 59, 59)] // Replace startDate and endDate with your actual values
+                }
+            },
+            order: [['date', 'ASC']]
+        });
+        res.json({
+            data: chart1Data.rows,
+            total: chart1Data.count,
+            error: false,
+            message: "OK - The request was successfull",            
+        });
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ error: "Internal Server Error" })
+    }
+})
+
+
 module.exports = {
     getAllVehicleCount,
     getToBeProcessedData,
-    getProcessedData
+    getProcessedData,
+    getLogData
 }
