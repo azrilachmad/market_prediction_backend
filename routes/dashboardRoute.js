af32fa7b6b0c540ae76cd8c12d0b3426aa4bcbdf
Azril Achmad <azrilachmad4@gmail.com>
2025-01-09 19:40:48 +0700

    Adjust dashboard API route

--- routes/dashboardRoute.js
+++ routes/dashboardRoute.js
@@ -1,10 +1,11 @@
 const { authentication, restrictTo } = require('../controllers/authController.js');
-const { getAllVehicleCount, getToBeProcessedData } = require('../controllers/dashboardController.js');
+const { getAllVehicleCount, getToBeProcessedData, getProcessedData } = require('../controllers/dashboardController.js');
 
 const router = require('express').Router()
 
 router.get('/api/dashboard/card1', authentication, getAllVehicleCount)
 router.get('/api/dashboard/card2', authentication, getToBeProcessedData)
+router.get('/api/dashboard/card3', authentication, getProcessedData)
 
 
 module.exports = router;
