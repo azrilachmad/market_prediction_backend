1ff68ffd12105f27b60353080900dfc579e8e186
Azril Achmad <azrilachmad4@gmail.com>
2025-01-21 19:22:55 +0700

    Adjust vehicle model database controller

--- model/vehicleModel.js
+++ model/vehicleModel.js
@@ -129,86 +129,8 @@ const Cars = db.define('Cars', {
         type: DataTypes.INTEGER,
         underscored: true,
     },
-
-
-
-    tanggal_jual: {
-        type: DataTypes.DATE,
-    },
-    lokasi: {
-        type: DataTypes.STRING,
-    },
-    jenismobil: {
-        type: DataTypes.STRING,
-    },
-    transmisi: {
-        type: DataTypes.STRING,
-    },
-    year: {
-        type: DataTypes.INTEGER,
-    },
-    umurmobil: {
-        type: DataTypes.INTEGER,
-    },
-    color: {
-        type: DataTypes.STRING,
-    },
-    nopol: {
-        type: DataTypes.STRING,
-    },
-    pajak: {
-        type: DataTypes.DATE,
-    },
-    stnk: {
-        type: DataTypes.DATE,
-    },
-    grade_all: {
-        type: DataTypes.STRING,
-        underscored: true,
-    },
-    gradeinterior: {
-        type: DataTypes.STRING,
-    },
-    gradebody: {
-        type: DataTypes.STRING,
-    },
-    grademesin: {
-        type: DataTypes.STRING,
-    },
-    km: {
-        type: DataTypes.INTEGER,
-    },
-    bottom_price: {
-        type: DataTypes.INTEGER,
-    },
-    status: {
-        type: DataTypes.STRING,
-    },
-    harga_terbentuk: {
-        type: DataTypes.INTEGER,
-        underscored: true
-    },
-    nama_mobil: {
-        type: DataTypes.STRING,
-        allowNull: true,
-    },
-    harga_atas: {
-        type: DataTypes.STRING,
-        allowNull: true,
-    },
-    harga_bawah: {
-        type: DataTypes.STRING,
-        allowNull: true,
-    },
-    hit_count: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-    },
-
-
-
 }, {
-    tableName: 'vehicle_price_check',
+    tableName: 'vehicle_price_check_ai',
     timestamps: true,
     updatedAt: 'updated_at',
     createdAt: 'created_at',
