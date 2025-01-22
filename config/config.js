e924aa9beb1437e3091677c46b9f3b44111eb98d
Azril Achmad <azrilachmad4@gmail.com>
2025-01-23 00:44:36 +0700

    Implement Search Filter on Vehicle Table List

--- config/config.js
+++ config/config.js
@@ -6,7 +6,7 @@ module.exports = {
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    "dialect": "postgres",
+    "dialect": "mysql",
     // "seederStorage": 'sequelize'
   },
   "production": {
@@ -15,7 +15,7 @@ module.exports = {
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
     "port": process.env.DB_PORT,
-    "dialect": "postgres",
+    "dialect": "mysql",
     // "seederStorage": 'sequelize',
     // Optional logging
     // logging: false,            // Set to true for debug logs; false for no logging
