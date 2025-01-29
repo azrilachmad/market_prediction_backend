6ba2207d4baf3936df19ffec9f6f8507d4935235
Azril Achmad <azrilachmad4@gmail.com>
2025-01-29 20:14:52 +0700

    Adjust update data harga atas harga bawah

--- index.js
+++ index.js
@@ -174,8 +174,8 @@ app.use(dashboardRoute);
 
                             await Cars.update(
                                 {
-                                    ai_harga_atas: !isNaN(resultData.harga_terendah) ? resultData.harga_terendah * 1 : 0,
-                                    ai_harga_bawah: !isNaN(resultData.harga_tertinggi) ? resultData.harga_tertinggi * 1 : 0,
+                                    ai_harga_atas: !isNaN(resultData.harga_tertinggi) ? resultData.harga_tertinggi * 1 : 0,
+                                    ai_harga_bawah: !isNaN(resultData.harga_terendah) ? resultData.harga_terendah * 1 : 0,
                                     hit_count: Sequelize.literal('hit_count + 1'),
                                 },
                                 { where: { id: data.id } }
